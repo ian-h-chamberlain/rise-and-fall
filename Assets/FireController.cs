a90341b3b988cba3ecf07a2230e91dcc1315d77d
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2015-11-25 17:10:33 -0500

    Added item prefab and preliminary destroy capsule and collecting mechanic. Also imported third person controller and other standard assets

---
+++ Assets/FireController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireController : MonoBehaviour {
+	
+	void OnTriggerStay (Collider other) {
+		if (Input.GetMouseButton(0) && other.gameObject.CompareTag("ItemCapsule")) {
+			Destroy(other.gameObject);
+		}
+	}
+
+	void Update() {
+		if (Input.GetMouseButton(0)) {
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
+		}
+		else {
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+		}
+	}
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	public int score = 0;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Item")) {
+			score++;
+			Destroy (other.gameObject);
+		}
+	}
+}
