9b41cb69fe2df2ef12fe7e4c63cf196f5727b1a8
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2015-11-27 19:51:32 -0500

    Added frequency property for collectible capsules and changed the way sound cone works

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireController : MonoBehaviour {
+
+	private bool isFiring;
+	private float curTime;
+	private int frequency;
+
+	public float fireTime;
+	public float scaleRate;
+	private Vector3 originalScale;
+
+	void Start() {
+		originalScale = gameObject.transform.localScale;
+		frequency = 2;
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (isFiring && other.gameObject.CompareTag("ItemCapsule/Caps" + frequency.ToString())) {
+			Destroy(other.gameObject);
+		}
+	}
+
+	void Update() {
+		// activate the firing process if the mouse button is pressed
+		if (Input.GetMouseButtonDown(0) && !isFiring) {
+			isFiring = true;
+			curTime = 0.0f;
+		}
+
+		if (Input.GetKeyDown("[")) {
+			frequency--;
+			Debug.Log ("frequency: " + frequency.ToString());
+		}
+		else if (Input.GetKeyDown("]")) {
+			frequency++;
+			Debug.Log ("frequency: " + frequency.ToString());
+		}
+
+		if (isFiring) {
+			// render the cone
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
+
+			Vector3 scale = gameObject.transform.localScale;
+			// scale the cone over time according to the scale rate
+			gameObject.transform.localScale = new Vector3(scale.x * scaleRate, scale.y * scaleRate, scale.z * scaleRate);
+
+			// add to the timer and check whether or not we should be done firing
+			curTime += Time.deltaTime;
+			if (curTime >= fireTime) {
+				isFiring = false;
+			}
+		}
+		else {
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+			gameObject.transform.localScale = originalScale;
+		}
+	}
+}
